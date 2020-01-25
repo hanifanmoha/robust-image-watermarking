6ff3f859873c1bfdc1e333f355cc4cbdf01e2d98
hanifanm <hanifanmohamad@gmail.com>
2020-01-25 16:54:11 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss'
+import React from 'react'
+import cx from 'classnames'
 
-function App() {
+const App = ({className}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={cx(className, styles.root)}>
+      Hello World
     </div>
   );
 }
