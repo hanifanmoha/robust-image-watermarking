0337fb9c09df16814cd1264ecf1827b210007f8a
hanifanm <hanifanmohamad@gmail.com>
2020-01-25 20:36:01 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "hanifanm.github.io/robust-image-watermarking/precache-manifest.f06d91727ace8aa3ffd07ce079da77b4.js"
+  "/robust-image-watermarking/precache-manifest.8484df89bbb613080aab014c60fe4b2f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("hanifanm.github.io/robust-image-watermarking/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robust-image-watermarking/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
